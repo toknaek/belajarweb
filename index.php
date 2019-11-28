3c0505cef4d9f851805dadad7926503b684d4205
toknaek <58311435+toknaek@users.noreply.github.com>
2019-11-29 03:51:56 +0700

    File Master

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>	
+
+<head>	
+	<title>andikas saputra</title>
+		<link rel="stylesheet" type="text/css" href="desain.css" >
+</head>
+
+<body>
+		<div class="header">	
+
+		</div>
+
+		<div class="menu">
+
+			
+			
+		</div>
+
+		<div class="content">
+			
+		</div>
+
+		<div class="bagi">
+				<div class="kiri">
+					Ini bagian Kiri	
+				</div>
+
+				<div class="kanan">
+					
+				</div>
+				
+
+		</div>
+
+		
+			
+
+		
+
+</body>
+
+</html>
